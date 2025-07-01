6bec68a193d6e11b6d6f7003c5256b5bb4cc0600
Vansh Gilhotra <vanshgilhotra8885@gmail.com>
2025-07-01 14:27:18 +0530

    Auth controller

---
+++ server/src/controllers/AuthController.ts
@@ -0,0 +1,51 @@
+import { Request,Response } from "express";
+import prisma from "../config/db.config.js";
+import jwt from "jsonwebtoken";
+
+interface LoginPayloadType{
+    name: string;
+    email: string;
+    provider: string;
+    oauth_id: string;
+    image?: string;
+}
+class AuthController{
+    static async login(req:Request, res:Response) {
+        try{
+            const body:LoginPayloadType = req.body;
+            let findUser = await prisma.user.findUnique({where: {
+                email: body.email
+            }})
+
+            if(!findUser) {
+                findUser = await prisma.user.create({
+                    data: {
+                        name: body.name,
+                        email: body.email,
+                        provider: body.provider,
+                        oauth_id: body.oauth_id,
+                        image: body.image
+                    }
+                });
+            }
+            let JWTPayload = {name:body.name,email:body.email,id:findUser.id};
+            const token = jwt.sign(JWTPayload, process.env.JWT_SECRET as string, {
+                expiresIn: "1d"
+            });
+            return res.json({
+                status: "success",
+                message: "Login successful",
+                data: {
+                    ...findUser,
+                    token: `Bearer ${token}`
+                }
+            })
+        }catch(error) {
+            return res.status(500).json({
+                message:"error with login backend "
+            });
+        }
+    }
+}
+
+export default AuthController;
