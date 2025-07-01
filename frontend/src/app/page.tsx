d8bf28c2fcbd779c4bb8bb0be979169dfa816469
Vansh Gilhotra <vanshgilhotra8885@gmail.com>
2025-07-01 11:41:47 +0530

    landing page done

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+import FeatureSection from "@/components/base/FeatureSection";
+import Footer from "@/components/base/Footer";
+import HeroSection from "@/components/base/HeroSection";
+import Navbar from "@/components/base/Navbar";
+import UserReviews from "@/components/base/UserReviews";
 
 export default function Home() {
   return (
-    <h1>
-      Hello there
-    </h1>
+     <div className="min-h-screen flex flex-col ">
+      {/* Header */}
+      <Navbar/>
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* Features Section */}
+      <FeatureSection />
+
+      {/* User Reviews Section */}
+      <UserReviews />
+
+      {/* Footer */}
+      <Footer />
+    </div>
   );
 }
