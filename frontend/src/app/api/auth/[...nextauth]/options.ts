57468fc30a85b6061a013c14eb18ea7081886bae
Vansh Gilhotra <vanshgilhotra8885@gmail.com>
2025-07-01 12:08:43 +0530

    OAuth login setup done

--- frontend/src/app/api/auth/[...nextauth]/options.ts
+++ frontend/src/app/api/auth/[...nextauth]/options.ts
@@ -1,20 +1,56 @@
-import { AuthOptions } from "next-auth";
+import { Account, AuthOptions, ISODateString } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
-export const authOption:AuthOptions = {
-    pages:{
-        signIn: "/"
-    },
-    providers:[
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-            authorization: {
-                params: {
-                    prompt: "consent",
-                    access_type: "offline",
-                    response_type: "code"
-                }
-            }
-        })
-    ]
+
+export interface CustomSession {
+  user?: CustomUser;
+  expires: ISODateString;
 }
+
+export interface CustomUser {
+  id?: string | null;
+  name?: string | null;
+  email?: string | null;
+  image?: string | null;
+  provider?: string | null;
+  token?: string | null;
+}
+
+export const authOption: AuthOptions = {
+  pages: {
+    signIn: "/",
+  },
+  callbacks:{
+    async signIn({ user, account}:{user: CustomUser,account:Account|null}) {
+        console.log("User signed in:", user);
+        console.log("Account provider:", account);
+        return true;
+    },
+    async session({ session, user,token}:{
+      session: CustomSession;
+      user: CustomUser;
+      token: JWT}){
+        session.user = user as CustomUser;
+        return session;
+    },
+    async jwt({ token, user }: { token: JWT; user?: CustomUser }) {
+        if (user) {
+            token.user = user;
+        }
+        return token;
+    }
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+};
