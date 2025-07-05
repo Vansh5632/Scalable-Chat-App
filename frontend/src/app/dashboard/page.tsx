3db67005b189656cfb56754c9276e89749acdcc9
Vansh Gilhotra <vanshgilhotra8885@gmail.com>
2025-07-05 22:43:25 +0530

    added the frontend for the group chat

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -3,9 +3,12 @@ import React from "react";
 import { authOption, CustomSession } from "../api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
 import CreateChat from "@/components/groupChat/CreateChat";
+import { fetchChatGroups } from "@/fetch/groupFetch";
 
 export default async function DashboardPage() {
+    
     const session:CustomSession|null = await getServerSession(authOption);
+    const groups:Array<ChatGroupType> | [] = await fetchChatGroups(session?.user?.token || "");
     return(
         <div>
             <DashNav name={session?.user?.name!} image={session?.user?.image ?? undefined}/>
---
+++ frontend/src/fetch/groupFetch.ts
@@ -0,0 +1,19 @@
+import { CHAT_GROUP_URL } from "@/lib/apiEndPoints";
+
+export async function fetchChatGroups(token: string) {
+    const res = await fetch(CHAT_GROUP_URL,{
+        headers:{
+            Authorization: token,
+        },
+        next:{
+            revalidate: 60*60,
+            tags: ["dashboard"],
+        },
+    });
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch chat groups");
+    }
+
+    const response = await res.json();+
+}
