f7e531e3a867b31b770cfa821f00d333654fb8c6
Vansh Gilhotra <vanshgilhotra8885@gmail.com>
2025-07-05 18:34:54 +0530

    chat group controller updated

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,6 +6,8 @@ const router = Router();
 
 router.post("/auth/login", AuthController.login);
 router.post("/chat-group", authMiddleware, ChatGroupController.store);
+router.get("/chat-group", authMiddleware, ChatGroupController.index);
+router.get("/chat-group/:id", authMiddleware, ChatGroupController.show);
 
 
 export default router;
